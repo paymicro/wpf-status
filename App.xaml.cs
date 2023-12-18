a9125b26f6c5fb30125f9d423ecf6f7f189e4a4e
Pavel Yakovlev <hronos2t@gmail.com>
2023-12-19 01:32:25 +0400

    minor

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ControlzEx.Theming;
+﻿using System.Diagnostics;
+using System.IO;
 using System.Windows;
 
 namespace WpfStatus
@@ -8,5 +9,23 @@ namespace WpfStatus
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            if (!File.Exists("grpcurl.exe"))
+            {
+                var request = MessageBox.Show("The grpcurl.exe tool is used for work. Download it from the github and put it in a folder.",
+                    "Need grpcurl",
+                    MessageBoxButton.OKCancel);
+
+                if (request == MessageBoxResult.OK)
+                {
+                    Process.Start("explorer", "https://github.com/fullstorydev/grpcurl/releases/latest");
+                }
+
+                this.Shutdown();
+            }
+        }
     }
 }
