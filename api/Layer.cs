e59d90790f1c5e9e07e01f81f0032a9838e18142
Pavel Yakovlev <hronos2t@gmail.com>
2023-12-25 22:18:05 +0400

    fix crash when soft by layer

--- api/Layer.cs
+++ api/Layer.cs
@@ -1,10 +1,19 @@
 ﻿namespace WpfStatus.api
 {
-    public class Layer: IComparable<Layer>
+    public class Layer: IComparable, IComparable<Layer>
     {
         public int Number { get; set; }
 
         public int CompareTo(Layer? other) => Number.CompareTo(other?.Number ?? 0);
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is Layer layer)
+            {
+                return CompareTo(layer);
+            }
+            return 0;
+        }
 
         public override string ToString() => Number.ToString();
     }
